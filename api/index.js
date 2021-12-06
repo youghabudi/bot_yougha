4b7df1c1feae47ce8985ccb30072d44d081c538d
youghabudi <95616983+youghabudi@users.noreply.github.com>
2021-12-06 17:48:27 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const cls_model = require('./sdk/cls_model.js'); //cls
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5057562264:AAFYkNhZ_IMT2GfjfpUg7lxzk--h09KKuRY'
+const token = '5031780127:AAHj04dMcBN0V7lp_XzB81kQSnS_HZWhrME'
 const bot = new TelegramBot(token, {polling: true});
 
 state=0;
